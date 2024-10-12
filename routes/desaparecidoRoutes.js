3d270d0cbdc01066a569189aae55875830b7ee42
WilliamBPFR <1104228@est.intec.edu.do>
2024-10-12 18:31:37 -0400

    Cambios en Guardar Publicacion

--- routes/desaparecidoRoutes.js
+++ routes/desaparecidoRoutes.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const desaparecidoController = require('../controllers/desaparecidoController');
+const {authenticate} = require('../routes/routeAutenthicationService');
+
+
+// Rutas Protegidas
+router.post('/desaparecido/crearDesaparecido', authenticate, desaparecidoController.createDesaparecido);
+
 
 router.get('/desaparecido/obtenerDesaparecido/:id', desaparecidoController.getDesaparecido);
 router.get('/desaparecido/obtenerDesaparecidos', desaparecidoController.getAllDesaparecidos);
-router.post('/desaparecido/crearDesaparecido', desaparecidoController.createDesaparecido);
 router.put('/desaparecido/updateDesaparecido/:id', desaparecidoController.updateDesaparecido);
 router.delete('/desaparecido/deleteDesaparecido/:id', desaparecidoController.deleteDesaparecido);
 
