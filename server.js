b5032a81fc792dd38edfc38a14da17f032734c95
MeSenku <102480963+WilliamBPFR@users.noreply.github.com>
2024-11-12 20:55:24 -0400

    Quitado Swagger

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = require('./app');
 const swaggerJsDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger-output.json'); // Cargar el archivo JSON generado
+// const swaggerDocument = require('./swagger-output.json'); // Cargar el archivo JSON generado
 
 const PORT = process.env.PORT || 5000;
 
 // Configurar Swagger UI
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(PORT, '0.0.0.0',() => {
     console.log(`Server running on port ${PORT}`);
