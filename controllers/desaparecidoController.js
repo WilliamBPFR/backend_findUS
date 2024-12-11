301e9b61253ca2ad541c55ae7d2f356445d9b3e8
Luis Adames <adames1601@gmail.com>
2024-12-11 19:27:42 -0400

    Merge branch 'dev-luis' of https://github.com/WilliamBPFR/backend_findUS into dev-luis

--- controllers/desaparecidoController.js
+++ controllers/desaparecidoController.js
@@ -181,6 +181,7 @@ const getDesaparecidosActivosScrollHorizontal = async (req, res) => {
     } catch (error) {
         console.log(error);
         res.status(500).json({ message: error.message });
+        //prueba
     }
 }
 
